6f4fce2bc35e6df3d67696f3123bd65a4ef214f2
Gokul Sujan <gokulsujan.dev@gmail.com>
2025-04-20 05:59:14 +0530

    Added a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginComponent from './components/login/Login'
 import { SnackbarComponent } from './components/utils/SnackbarComponent.jsx'
 import SignupComponent from "./components/signup/Signup.jsx";
+import NavbarComponent from "./components/home/Navbar.jsx";
 
 function App() {
     return (
         <>
+            <NavbarComponent />
             <SnackbarComponent>
                 <Router>
                     <Routes>
