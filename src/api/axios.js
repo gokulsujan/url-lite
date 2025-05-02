4c335caccef320283e38fbdb2f7389e1e8f05249
Gokul Sujan <gokulsujan.dev@gmail.com>
2025-05-02 17:35:27 +0530

    Fixing unwanted redirect

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,7 +8,7 @@ const api = axios.create({
 api.interceptors.response.use(
     response => response,
     error => {
-      if (error.response?.status === 401 || localStorage.getItem('access_token') == null) {
+      if (error.response?.status === 401) {
         localStorage.removeItem('access_token');
         window.location.href = '/signin';
       }
